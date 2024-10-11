efdea5cff47b27fc55a75e1112aa8bd826141add
Yermehyaw <yermehyaw@gmail.com>
2024-10-11 20:49:20 +0100

    Fix pycodestyle errs

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -23,7 +23,7 @@ def canUnlockAll(boxes):
 
     for box in boxes:
         # get the keys from each box and save to a set
-        keys = box;
+        keys = box
         available_keys.update(keys)
 
     # sort the unique keys in the set
@@ -31,13 +31,16 @@ def canUnlockAll(boxes):
     # print(sorted_keys)
 
     # create an ideal list of what the keys should be
-    all_keys = [(no_boxes - 1) for i in range(no_boxes)]  ###
+    all_keys = [(no_boxes - 1) for i in range(no_boxes)]  #
     all_keys.sort()
     # print(all_keys)
 
-    if len(sorted_keys) == (no_boxes - 1):  # we know we have same no unique keys as with the no of boxes
-        # but are these unique keys capable of opeming each box i.e do they match each individual box?, asides the already opened 0th box)
+    if len(sorted_keys) == (no_boxes - 1):
         """
+        # we know we have same no unique keys as with the no of boxes
+        # but are these unique keys capable of opeming each box i.e do they
+        match each individual box?, asides the already opened 0th box)
+
         i = 0
         for key in sorted_keys:
             if key == 0 and (key not in sorted_keys):
