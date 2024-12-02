9e6d719a760a64ad430a4e6548cb0a53b2f03e7e
Yermehyaw <yermehyaw@gmail.com>
2024-12-03 00:43:36 +0100

    Using recursion

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,7 +9,31 @@ maths.min
 
 
 def makeChange(coins, total):
-    """Making change using arbitrary coin denominations
+    """Returns the least no of change for total using denominations in coins
     """
     if not coins or total <= 0:
         return 0
+
+    # Iterative
+    count = 0
+    for coin in coins:
+        if coin <= total:
+            temp = total - coin
+            count += 1
+
+        if temp == 0:
+            return count
+
+    return -1
+
+    # recursive
+    if total <= 0:
+        return total
+
+    for i in range(len(coins)):
+        total = total - coins[i]
+        makeChange(coins, total)
+        if total == 0:
+            return i
+
+    return -1
