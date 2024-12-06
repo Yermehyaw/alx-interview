76497805f7929a2288f7a5a5b8f386a7dee85615
Yermehyaw <yermehyaw@gmail.com>
2024-12-06 04:56:09 +0000

    Use diff algo to subtract from ideal perimeter

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -26,20 +26,32 @@ def island_perimeter(grid):
     # evaluate coordinates where there is earth to derive perim of land
     earth_no = len(earth)
     perimeter = 4 * earth_no  # ideal permieter if each were separate
+
     # find matching coordinates and deduct from ideal perimeter
+    j = 0
     for i in range(earth_no):
-        j = 1  # greater than i by 1 step
-        if earth[i][0] == earth[i][1]:  # each row has an index of 0 & 1
+        j += 1 # greater than i by 1 step
+        if j == earth_no:  # prevent IndexError
+            break
+        # 0 & 1 are the x & y co-ordinates respt
+        if earth[i][0] == earth[j][0]:  # share the same x co-ordinate
+            if earth[j][1] == (earth[i][1] + 1):  # do they touch horizontally?
+                perimeter -= 2
+        if earth[i][1] == earth[j][1]:  # share the same y co-ordinate
+            if earth[j][0] == (earth[i][0] + 1): ###
+                perimeter -= 2
+        """
+        if earth[i][0] == earth[i][1]:  # 0 & 1 are the x & y co-ordinates respt
             perimeter -= 1
+        
         if earth[i][1] == earth[i][0]:
             perimeter -= 1
 
-        """"
         if earth[j][0] == earth[j][1]:
             perimeter -= 1
         if earth[j][1] == earth[j][0]:
             perimeter -= 1
-        """
+        
 
         if earth[i][0] == earth[j][0]:
             perimeter -= 1
@@ -49,6 +61,8 @@ def island_perimeter(grid):
             perimeter -= 1
         if earth[j][0] == earth[i][0]:
             perimeter -= 1
-        j += 0
+
+        j += 1
+        """
 
     return perimeter
